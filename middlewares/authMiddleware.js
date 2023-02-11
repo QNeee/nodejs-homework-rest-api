12db4c66c4a3f2a9b06b59809643bc2348f8f828
QNeee <vovasagan7@gmail.com>
2023-02-11 15:46:48 +0300

    auth,contacts=done

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,17 +3,17 @@ const { NotAuthorized } = require('../helpers/errors')
 const { User } = require('../db/userModel');
 
 const authMiddleware = async (req, res, next) => {
-    const [tokenType, token] = req.headers.authorization.split(' ');
-    if (!token) {
-        next(new NotAuthorized('Please ,provide a token'));
-    }
     try {
+        const [tokenType, token] = req.headers.authorization.split(' ');
+        if (!token) {
+            next(new NotAuthorized('Please ,provide a token'));
+        }
         const user = jwt.decode(token, process.env.JWT_SECRET);
         req.token = token;
         req.user = user;
         next();
     } catch (error) {
-        throw new NotAuthorized('Invalid Token')
+        next(new NotAuthorized('Invalid Token'));
     }
 }
 
